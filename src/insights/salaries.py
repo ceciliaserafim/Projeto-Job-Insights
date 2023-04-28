071af7294620e9322a38f3967f7ce486528ac45f
Cecília Serafim <ceciliaserafimti@gmail.com>
2023-04-28 18:18:16 -0300

    Adiciona requisito 5

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -11,21 +11,11 @@ def get_max_salary(path: str) -> int:
 
 
 def get_min_salary(path: str) -> int:
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    data = read(path)
+    salary_min = [
+        int(row["min_salary"])
+        for row in data if row["min_salary"].isdigit()]
+    return min(salary_min)
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
