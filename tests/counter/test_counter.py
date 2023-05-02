133be07b7f730f1e82274c1b287ba3dc27f4b8b0
Cecília Serafim <ceciliaserafimti@gmail.com>
2023-05-02 10:30:45 -0300

    Adiciona requisito 10 testes

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+
+# testa quantas vezes a palavra word aparece
 
 
 def test_counter():
-    pass
+    path = "data/jobs.csv"
+    word = "word"
+
+    assert count_ocurrences(path, word) == 382
