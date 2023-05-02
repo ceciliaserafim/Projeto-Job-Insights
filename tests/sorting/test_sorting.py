d69ca7d8427faa107e157c187795802e3fecdb35
Cecília Serafim <ceciliaserafimti@gmail.com>
2023-05-02 12:02:39 -0300

    Adiciona requisito 11 teste - test_sort_by_criteria

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,37 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
 
 
-def test_sort_by_criteria():
-    pass
+jobs = [
+    {'max_salary': '3812', 'min_salary': '1984', 'date_posted': '2020-05-02'},
+    {'max_salary': '4000', 'min_salary': '1000', 'date_posted': '2021-05-02'},
+    {'max_salary': '5000', 'min_salary': '2000', 'date_posted': '2022-05-02'},
+    ]
+
+job_max_salary = [
+    {'max_salary': '5000', 'min_salary': '2000', 'date_posted': '2022-05-02'},
+    {'max_salary': '4000', 'min_salary': '1000', 'date_posted': '2021-05-02'},
+    {'max_salary': '3812', 'min_salary': '1984', 'date_posted': '2020-05-02'},
+    ]
+
+job_min_salary = [
+    {'max_salary': '4000', 'min_salary': '1000', 'date_posted': '2021-05-02'},
+    {'max_salary': '3812', 'min_salary': '1984', 'date_posted': '2020-05-02'},
+    {'max_salary': '5000', 'min_salary': '2000', 'date_posted': '2022-05-02'},
+    ]
+
+job_date_posted = [
+    {'max_salary': '5000', 'min_salary': '2000', 'date_posted': '2022-05-02'},
+    {'max_salary': '4000', 'min_salary': '1000', 'date_posted': '2021-05-02'},
+    {'max_salary': '3812', 'min_salary': '1984', 'date_posted': '2020-05-02'},
+    ]
+
+
+def test_sort_by_criteria():   
+    sort_by(jobs, 'max_salary')
+    assert jobs == job_max_salary
+    
+    sort_by(jobs, 'min_salary')
+    assert jobs == job_min_salary
+
+    sort_by(jobs, 'date_posted')
+    assert jobs == job_date_posted
